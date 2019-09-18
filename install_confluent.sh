7f8cacfd40564654ea3b83c02d6e9540cde6ce38
Joshua Yeung <joshua.chyeung@gmail.com>
2019-09-18 10:50:35 +0800

    add install docker-compose

--- install_confluent.sh
+++ install_confluent.sh
@@ -4,5 +4,10 @@ git checkout 5.3.1-post
 
 cd cp-all-in-one/
 
+# install docker-compose
+sudo curl -x 172.19.1.179:3128 -L "https://github.com/docker/compose/releases/download/1.24.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+
+
+
 docker-compose up -d --build
 
